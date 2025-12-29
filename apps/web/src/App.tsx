f5c8aa7b63c18a1107a0bc5c0b8cc1d61ece5cc5
Mayank Gupta <mayank.gupta@cube.ms>
2025-12-29 14:25:25 +0530

    Shadcn UI installed and working fine

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
-const App = () => {
-  return <div className="text-3xl font-bold underline">Hello world!</div>;
-};
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
 
 export default App;
