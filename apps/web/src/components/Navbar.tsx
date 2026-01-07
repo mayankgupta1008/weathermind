0123e500aae3a6719778f54a85d51577e904f202
Mayank Gupta <mayank.gupta@cube.ms>
2026-01-07 17:50:20 +0530

    fixed tailwind css issue for v4

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,5 +1,24 @@
+import logo from "@/assets/logo.png";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-black/20 dark:border-white/30 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-16 items-center justify-between px-4 md:px-6 mx-auto max-w-7xl">
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <Link to="/" className="flex items-center gap-2">
+            <img
+              src={logo}
+              alt="Description"
+              className="w-10 h-10 object-contain"
+            />
+            <span className="font-bold sm:inline-block">Weather Agent</span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
